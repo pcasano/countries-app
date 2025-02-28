a7a3337154817bda95f208431b16170682f414d6
pcasano <pc.learnings@pm.me>
2025-02-28 23:32:03 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {CountryService} from './services/country.service';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
+  providers: [CountryService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'countries-app';
+export class AppComponent implements OnInit {
+
+  private countryService = inject(CountryService);
+  countries: any[] = [];
+
+  ngOnInit(): void {
+    this.countryService.getAllCountries().subscribe(
+      (data) => {
+        this.countries = data;
+        console.log(this.countries)
+      }
+    );
+  }
+
 }
